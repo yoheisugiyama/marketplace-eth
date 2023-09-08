a8e1186986166a83be0ba6b4e148904368dd5db6
Yohei Sugiyama <yoheisugiyamameister@gmail.com>
2023-09-09 03:41:38 +0900

    112

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,24 @@
 import { Hero, Breadcrumbs } from "@components/common"
 import { CourseList } from "@components/course"
 import { BaseLayout } from "@components/layout"
-import { OrderCard } from "@components/order"
-import { EthRates, WalletBar } from "@components/web3"
+import { getAllCourses } from "@content/courses/fetcher"
 
-export default function Home() {
+export default function Home({ courses }) {
   return (
     <>
       <Hero />
-      <Breadcrumbs />
-      <WalletBar />
-      <EthRates />
-      <OrderCard />
-      <CourseList />
+      <CourseList courses={courses} />
     </>
   )
+}
+
+export function getStaticProps() {
+  const { data } = getAllCourses()
+  return {
+    props: {
+      courses: data,
+    },
+  }
 }
 
 Home.Layout = BaseLayout
