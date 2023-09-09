d360a5c9e94f5b457c67cf3bc9764814ab988329
Yohei Sugiyama <yoheisugiyamameister@gmail.com>
2023-09-10 05:50:40 +0900

    120

--- components/ui/layout/base/index.js
+++ components/ui/layout/base/index.js
@@ -1,13 +1,14 @@
-import { Footer, Navbar } from "@components/common"
+import { Footer, Navbar } from "@components/ui/common"
+import { Web3Provider } from "@components/providers"
 
 export default function BaseLayout({ children }) {
   return (
-    <>
+    <Web3Provider>
       <div className="max-w-7xl mx-auto px-4">
         <Navbar />
         <div className="fit">{children}</div>
       </div>
       <Footer />
-    </>
+    </Web3Provider>
   )
 }
