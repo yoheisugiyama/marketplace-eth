02769d28e6283a0bcd02ab94f58849def9aa279b
Yohei Sugiyama <yoheisugiyamameister@gmail.com>
2023-09-23 17:01:09 +0900

    139

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -1,8 +1,12 @@
 import { useEffect } from "react"
 import useSWR from "swr"
 
+const adminAddresses = {
+  "0x3a0a50243a16ebdf953e73e963eab6acc01ea7bdfdbbf8b9cf7db1ea2ef9cc82": true,
+}
+
 export const handler = (web3, provider) => () => {
-  const { mutate, ...rest } = useSWR(
+  const { data, mutate, ...rest } = useSWR(
     () => (web3 ? "web3/accounts" : null),
     async () => {
       const accounts = await web3.eth.getAccounts()
@@ -15,5 +19,12 @@ export const handler = (web3, provider) => () => {
       provider.on("accountsChanged", (accounts) => mutate(accounts[0] ?? null))
   }, [provider])
 
-  return { account: { mutate, ...rest } }
+  return {
+    account: {
+      data,
+      isAdmin: (data && adminAddresses[web3.utils.keccak256(data)]) ?? false,
+      mutate,
+      ...rest,
+    },
+  }
 }
--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,11 +1,13 @@
 import { useWeb3 } from "@components/providers"
 import Link from "next/link"
 import { Button } from "@components/ui/common"
-import { useAccount } from "@components/web3/hooks/useAccount"
+import { useAccount } from "@components/hooks/web3/useAccount"
+import { useRouter } from "next/router"
 
 export default function Navbar() {
   const { connect, isLoading, isWeb3Loaded } = useWeb3()
   const { account } = useAccount()
+  const { pathname } = useRouter()
 
   return (
     <section>
@@ -20,7 +22,7 @@ export default function Navbar() {
                 Home
               </Link>
               <Link
-                href="/"
+                href="/marketplace"
                 className="font-medium mr-8 text-gray-500 hover:text-gray-900"
               >
                 Marketplace
@@ -46,7 +48,7 @@ export default function Navbar() {
               ) : isWeb3Loaded ? (
                 account.data ? (
                   <Button hoverable={false} className="cursor-default">
-                    Hi There!
+                    Hi There {account.isAdmin && "Admin"}
                   </Button>
                 ) : (
                   <Button onClick={connect}>Connect</Button>
@@ -64,7 +66,7 @@ export default function Navbar() {
           </div>
         </nav>
       </div>
-      {account.data && (
+      {account.data && !pathname.includes("/marketplace") && (
         <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
           <div className="text-white bg-indigo-600 rounded-md p-2">
             {account.data}
