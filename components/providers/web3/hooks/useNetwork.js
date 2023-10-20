22f6aeb5a9f284e6e36be64a83b63f60f2215090
Yohei Sugiyama <yoheisugiyamameister@gmail.com>
2023-10-20 22:13:38 +0900

    147

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -15,7 +15,7 @@ const targetNetwork = NETWORKS[process.env.NEXT_PUBLIC_TARGET_CHAIN_ID]
 
 
 export const handler = (web3, provider) => () => {
-  const { data, error, mutate, ...rest } = useSWR(
+  const { data, mutate, ...rest } = useSWR(
     () => (web3 ? "web3/network" : null),
     async () => {
       const chainId = await web3.eth.getChainId()
@@ -36,14 +36,13 @@ export const handler = (web3, provider) => () => {
 
 
   return {
-    network: {
-      data,
-      hasFinishedFirstFetch: data || error,
-      mutate,
-      target: targetNetwork,
-      isSupported: data === targetNetwork,
-      ...rest,
-    },
+
+    data,
+    mutate,
+    target: targetNetwork,
+    isSupported: data === targetNetwork,
+    ...rest,
+
 
   }
 
--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -2,8 +2,8 @@ import { CourseCard, CourseList } from "@components/ui/course"
 import { BaseLayout } from "@components/ui/layout"
 import { getAllCourses } from "@content/courses/fetcher"
 import { WalletBar } from "@components/ui/web3"
-import { useAccount } from "@components/hooks/web3/useAccount"
-import { useNetwork } from "@components/hooks/web3/useNetwork"
+import { useAccount, useNetwork } from "@components/hooks/web3"
+import { Modal } from "@components/ui/common"
 
 export default function Marketplace({ courses }) {
   const { account } = useAccount()
@@ -21,7 +21,7 @@ export default function Marketplace({ courses }) {
             data: network.data,
             target: network.target,
             isSupported: network.isSupported,
-            hasFinishedFirstFetch: network.hasFinishedFirstFetch
+            hasInitialResponse: network.hasInitialResponse
           }}
 
         />
@@ -29,6 +29,7 @@ export default function Marketplace({ courses }) {
       <CourseList courses={courses}>
         {(course) => <CourseCard key={course.id} course={course} />}
       </CourseList>
+      <Modal></Modal>
     </>
   )
 }
